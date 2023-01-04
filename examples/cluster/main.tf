d46951215211ad6a8e90395de4b4382cd08ddba4
Sebastian Struß <struss@justtrack.io>
2023-01-04 14:18:28 +0100

    fix: Changed region to aws_region

--- examples/cluster/main.tf
+++ examples/cluster/main.tf
@@ -11,7 +11,7 @@ module "example" {
   vpc_id         = "vpc-1245f04ef5690379a"
   initial_vpc_id = "vpc-2c1a11081fd686d0d"
   subnet_ids     = ["subnet-04016197c1fb0e3bf", "subnet-0540fa26a2ae5553a", "subnet-012ca55fcddf605af"]
-  region         = "eu-central-1"
+  aws_region     = "eu-central-1"
   stage          = "stage"
   environment    = "env"
 }

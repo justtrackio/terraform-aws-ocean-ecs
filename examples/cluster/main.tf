8c9f70406840de557caf6f06d85b488e7d555ced
mfroembgen <user.personal@users.noreply.github.com>
2023-01-04 14:25:53 +0100

    feat: Adjust_provider_config

--- examples/cluster/main.tf
+++ examples/cluster/main.tf
@@ -10,6 +10,6 @@ module "example" {
   vpc_id                             = "vpc-00000000000000000"
   initial_vpc_id                     = "vpc-00000000000000000"
   subnet_ids                         = ["subnet-00000000000000000", "subnet-11111111111111111", "subnet-22222222222222222"]
-  region                             = "eu-central-1"
+  aws_region                         = "eu-central-1"
   service_discovery_private_dns_name = "foo.baa"
 }
